ab4c96295437df66ea686cf7107d1f0f7db8881f
ekater1na <ekaterina_l@tut.by>
2022-07-04 19:43:53 +0300

    feat: add module

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -16,7 +16,6 @@ import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
