3cb6fa2d49c21c9084d1f6c9ec8a4298a030cb99
ekater1na <ekaterina_l@tut.by>
2022-07-11 23:31:24 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Products example')
+    .setDescription('The products API description')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(PORT);
+
+  console.log(`
+  🚀 Server running on http://localhost:${PORT}
+  👌 Swagger: http://localhost:3000/api`);
 }
 bootstrap();
